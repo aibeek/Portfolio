7024b99928983d05fe26e9c40fe379340bfe92d8
daulet <dauka2707@gmail.com>
2024-07-06 12:39:51 +0600

    git

--- portfolio/src/components/navbar/index.jsx
+++ portfolio/src/components/navbar/index.jsx
@@ -1,9 +1,24 @@
-import './style.css'
+import { useState } from 'react';
+import { getImageUrl } from '../../utils';
+import './style.css';
+
 const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className='navbar'>
             <a className='title' href="/">Portfolio</a>
             <div className='menu'>
+                <img
+                    className="menu-btn"
+                    src={
+                        menuOpen ?
+                            getImageUrl('nav/closeIcon.png') :
+                            getImageUrl('nav/menuIcon.png')}
+                    alt="menu-button"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                />
                 <ul className='menu-items'>
                     <li>
                         <a href="#about">About</a>
